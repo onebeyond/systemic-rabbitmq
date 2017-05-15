3612db0ab9de2641b773015bd8cecb4044cbc575
feliun <felipe.polo@gmail.com>
2017-05-15 23:36:23 +0200

    Minimal broker creation

---
+++ index.js
@@ -0,0 +1,43 @@
+var async = require('async')
+var get = require('lodash.get')
+var has = require('lodash.has')
+
+module.exports = function(options) {
+
+    var RabbitClient = get(options, 'RabbitClient') || require('rascal')
+    var broker
+    var config
+    var logger
+
+    function init(dependencies, cb) {
+        config = dependencies.config
+        logger = dependencies.logger || console
+        cb()
+    }
+
+    function validate(cb) {
+        if (!has(config, 'vhosts')) return cb(new Error('config.vhosts is required'))
+        cb()
+    }
+
+    function start(cb) {
+        logger.info('Connecting to rabbitmq')
+        var conf = RabbitClient.withDefaultConfig(config);
+
+        RabbitClient.createBroker(conf, options.components || {}, function(err, _broker) {
+            broker = _broker;
+            cb(err, { broker: broker });
+        })
+    }
+
+    function stop(cb) {
+        if (!broker) return cb()
+        logger.info('Disconnecting rabbitmq')
+        broker.shutdown(cb)
+    }
+
+    return {
+        start: async.seq(init, validate, start),
+        stop: stop
+    }
+}
